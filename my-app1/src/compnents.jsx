4192e7c399ac7bd61d0548c6f979cb32ea859bdd
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2024-05-10 16:15:36 +0500

    events

--- my-app1/src/compnents.jsx
+++ my-app1/src/compnents.jsx
@@ -15,8 +15,37 @@ function Link() {
 
 function Span() {
     return (
-       <span><i>Hello, this is a span</i></span>
+        <span><i>Hello, this is a span</i></span>
+    )
+
+}
+
+function AlertButton({name, message}) {
+
+    return (
+        <>
+            <button onClick={()=> alert(message) } >{name}</button></>
+    )
+
+}
+
+function Events() {
+    const numbers = 2 + 2
+
+    function ClickButton() {
+        alert("This is a alert")
+    }
+
+
+    return (
+        <>
+            <h2>This Result is {numbers}</h2>
+            {/*<button onClick={ClickButton}>Click Me</button>
+            <button onClick={() => alert('This is a auto function')} >Auto Function</button> */}
+            <AlertButton name="Play" message="This is a Play Button" />
+            <AlertButton name="Click" message="This is a Click Button" />
+        </>
     )
 }
 
-export { Link,Span }
+export { Link, Span, Events, AlertButton }
--- my-app1/src/main.jsx
+++ my-app1/src/main.jsx
@@ -7,18 +7,20 @@ import About from './About.jsx'
 import Profile from './Profile.jsx'
 import user from '/user.webp'
 import Products from './Products.jsx'
+import { Events } from './compnents.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+
   <React.StrictMode>
     {/* components */}
+    <Events />
     <Profile myname="Muddasir" age="19" city="Karachi" profile={user} />
     {/* <Profile age="19" city="Karachi" profile={user} /> */}
     <Home />
     <About />
     <Profile myname="Asim" age="21" city="Multan" profile={user} />
     <Profile myname="Neha" age="20" city="Lahore" profile={user} />
-    <Products/>
+    <Products />
   </React.StrictMode>,
 
 
