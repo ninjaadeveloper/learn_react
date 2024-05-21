5f66abef351d18de1f253b1a56936e7d845ce7a6
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2024-05-21 04:04:58 -0700

    tailwind and thinking

---
+++ react_tailwind/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <div className='bg-orange-500 rounded-full shadow-2xl shadow-orange-500'>
+        <h1 className='text-slate-100 text-7xl font-bold p-12'>Tailwind Css</h1>
+      </div> */}
+    </>
+  )
+}
+
+export default App
---
+++ react_thinking/src/FilterTable.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import SearchBar from "./Seach";
+import ProductTable from "./ProducTable";
+
+export default function FilterableProductTable({ products }) {
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+
+    return (
+        <div>
+            <SearchBar
+                filterText={filterText}
+                inStockOnly={inStockOnly}
+                onFilterTextChange={setFilterText}
+                onInStockOnlyChange={setInStockOnly} />
+            <ProductTable
+                products={products}
+                filterText={filterText}
+                inStockOnly={inStockOnly} />
+        </div>
+    );
+}
+
